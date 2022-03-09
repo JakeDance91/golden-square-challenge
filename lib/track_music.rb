9abb23241a07c7a80f7fb79f5953c6010a6b8171
Jake Dance <jake.dance1991@gmail.com>
2022-03-09 17:15:25 +0000

    removed comments from track_music.rb

--- lib/track_music.rb
+++ lib/track_music.rb
@@ -1,5 +1,3 @@
-# add artist and song - push to hash with key(artist) and key(song)
-# as both will be added at the same time, can call upon them to be returned as a list? indexed. 
 class Track_Music
   def initialize
     @library = []
